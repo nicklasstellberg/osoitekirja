ee541a9b6fd00ef99a6a99d559950dbad77ba3b2
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-05-18 14:04:50 +0300

    send address and cordinates via email

--- MapScreen.js
+++ MapScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Linking } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { Button } from "@rneui/themed";
 
 const MapScreen = ({ route }) => {
   const { address } = route.params;
@@ -30,23 +31,41 @@ const MapScreen = ({ route }) => {
       });
   }, [address]);
 
+  const handleEmailButtonPress = () => {
+    const emailSubject = 'Sijainti';
+    const emailBody = `Osoite: ${address}\nKoordinaatit: ${region.latitude}, ${region.longitude}`;
+
+    Linking.openURL(`mailto:?subject=${emailSubject}&body=${emailBody}`);
+  };
+
   return (
     <View style={styles.container}>
-      {region ? (
-        <MapView
-          style={styles.map}
-          region={region}
-          initialRegion={region}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-        >
-          <Marker coordinate={region} title={address} />
-        </MapView>
-      ) : (
-        <View style={styles.messageContainer}>
-          <Text style={styles.messageText}>
-            {locationFound ? 'Ladataan karttaa...' : 'Osoitetta ei löytynyt.'}
-          </Text>
+      <View style={styles.mapContainer}>
+        {region ? (
+          <MapView
+            style={styles.map}
+            region={region}
+            initialRegion={region}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+          >
+            <Marker coordinate={region} title={address} />
+          </MapView>
+        ) : (
+          <View style={styles.messageContainer}>
+            <Text style={styles.messageText}>
+              {locationFound ? 'Ladataan karttaa...' : 'Osoitetta ei löytynyt.'}
+            </Text>
+          </View>
+        )}
+      </View>
+
+      {region && (
+        <View style={styles.buttonContainer}>
+          <Button raised
+          icon={{
+            name: "mail",
+          }} title="Lähetä osoite" onPress={handleEmailButtonPress} />
         </View>
       )}
     </View>
@@ -55,20 +74,28 @@ const MapScreen = ({ route }) => {
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapContainer: {
+    flex: 1,
+    width: '100%',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
   messageContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   messageText: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginVertical: 20,
   },
 });
 
